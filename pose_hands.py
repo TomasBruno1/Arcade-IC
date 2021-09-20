417cdeac85b73bc28d637c6fd4a7a7144c9e475d
catamendizabal <catalina.mendizabal@ing.austral.edu.ar>
2021-09-20 14:39:08 -0300

    Improved performance

--- pose_hands.py
+++ pose_hands.py
@@ -1,10 +1,7 @@
-import numpy as np
 import cv2
 import mediapipe as mp
-import pyautogui
-import pydirectinput as keyboard_controller
+import pyautogui as keyboard_controller
 
-# keyboardController = Controller()
 
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
@@ -40,10 +37,6 @@ def release_key(key):
     return False
 
 
-def get_distance(p, q):
-    return np.sqrt((q.x - p.x)**2 + (q.y - p.y)**2)
-
-
 with mp_holistic.Holistic(
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5) as holistic:
@@ -51,27 +44,14 @@ with mp_holistic.Holistic(
         success, image = cap.read()
         if not success:
             print("Ignoring empty camera frame.")
-            # If loading a video, use 'break' instead of 'continue'.
             continue
 
-        # Flip the image horizontally for a later selfie-view display, and convert
-        # the BGR image to RGB.
         image = cv2.cvtColor(cv2.flip(image, 1), cv2.COLOR_BGR2RGB)
-        # To improve performance, optionally mark the image as not writeable to
-        # pass by reference.
         image.flags.writeable = False
         results = holistic.process(image)
 
-        # Draw landmark annotation on the image.
         image.flags.writeable = True
         image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-        # mp_drawing.draw_landmarks(
-        #     image,
-        #     results.face_landmarks,
-        #     mp_holistic.FACEMESH_CONTOURS,
-        #     landmark_drawing_spec=None,
-        #     connection_drawing_spec=mp_drawing_styles
-        #         .get_default_face_mesh_contours_style())
         mp_drawing.draw_landmarks(
             image,
             results.pose_landmarks,
@@ -123,14 +103,6 @@ with mp_holistic.Holistic(
                     0.25 < rightHand.x < 0.75 and 0.25 < rightHand.y < 0.75):
                 colorR = 255, 0, 255
                 textR = "none"
-
-            leftFingerTip = results.left_hand_landmarks.landmark[12]
-            rightFingerTip = results.right_hand_landmarks.landmark[12]
-            leftFingerMcp = results.left_hand_landmarks.landmark[9]
-            rightFingerMcp = results.right_hand_landmarks.landmark[9]
-
-            leftDistance = get_distance(leftFingerMcp, leftFingerTip)
-            rightDistance = get_distance(rightFingerMcp, rightFingerTip)
 
         cv2.rectangle(image, (100, 100), (300, 300), colorR, cv2.FILLED)
         cv2.putText(image, textR, (120, 220), cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 0))
