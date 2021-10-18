f586ecb8c2bfdf563087f43d713028701bb7e325
catamendizabal <catalina.mendizabal@ing.austral.edu.ar>
2021-10-18 17:00:58 -0300

    register integration

--- front/src/screens/login/LoginPage.js
+++ front/src/screens/login/LoginPage.js
@@ -1,8 +1,8 @@
-import {Box, Button, Divider} from "@material-ui/core";
+import {Box, Button, Divider, FormHelperText} from "@material-ui/core";
 import {useHistory} from "react-router-dom";
 import './LoginPage.css';
 import {FormGroup, Input} from 'reactstrap';
-import {useState} from "react";
+import React, {useState} from "react";
 import {WebcamCapture} from '../../components/webcam/webcam'
 import {userAPI} from "../../apis/userAPI";
 
@@ -12,16 +12,26 @@ const LoginPage = () => {
     const history = useHistory();
 
     const [username, setUsername] = useState("")
+    const [usernameError, setUsernameError] = useState("")
     const [picture, setPicture] = useState()
-
+    const [pictureError, setPictureError] = useState("")
 
     const onSubmit = () => {
-        const formData = new FormData();
-        formData.append("username", username)
-        formData.append("image", picture)
-        userAPI.loginData(formData).then((response) =>  {
-            console.log(response)
-        })
+        if (username === "") {
+            setUsernameError("* Username is required")
+        }
+        if (picture === undefined) {
+            setPictureError("* Picture is required")
+        }
+        if(false) {
+            const formData = new FormData();
+            formData.append("username", username)
+            formData.append("image", picture)
+            userAPI.loginData(formData).then((response) =>  {
+                console.log(response)
+            })
+        }
+
     }
 
     return (
@@ -38,9 +48,12 @@ const LoginPage = () => {
                             <FormGroup>
                                 <Input id="custom-button" placeholder=" Username"
                                        onChange={(text) => setUsername(text.target.value)}/>
+                                <FormHelperText id='helper-text' error>{!!usernameError ? usernameError : ' '}</FormHelperText>
                             </FormGroup>
                         </div>
                         <WebcamCapture setPicture={setPicture} user={username}/>
+                        <FormHelperText id='picture-text' error>{!!pictureError ? pictureError : ' '}</FormHelperText>
+
                         <div className='suggest-text'>Do not have an account?
                             <span className='link-text'
                                   onClick={() => history.push('/register')}>Register in here!</span>
